ef14ee8bdae08f4c0e4d1c47b7736878793dd828
apollo78124 <98apollo@naver.com>
2023-04-26 20:12:06 -0700

    Read command

--- CommandProcessor.cpp
+++ CommandProcessor.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
 #include "CommandProcessor.h"
 using namespace std;
 
@@ -13,6 +15,18 @@ namespace GIS {
 
     }
 
+    void tokenize(std::string const &str, const char delim,
+                  std::vector<std::string> &out)
+    {
+        // construct a stream from the string
+        std::stringstream ss(str);
+
+        std::string s;
+        while (std::getline(ss, s, delim)) {
+            out.push_back(s);
+        }
+    }
+
     int CommandProcessor::readScript()
     {
         string myText;
@@ -21,7 +35,14 @@ namespace GIS {
         // Use a while loop together with the getline() function to read the file line by line
         while (getline (ScriptFile1, myText)) {
             // Output the text from the file
-            cout << myText;
+            string command = "";
+            if (myText.length() > 0) {
+                if (myText[0] != ';') {
+                    vector<string> concatenated;
+                    tokenize(myText, '\t', concatenated);
+                    command = concatenated[0];
+                }
+            }
         }
         ScriptFile1.close();
         return 0;
