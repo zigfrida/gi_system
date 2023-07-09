57e3530c0c3201ef86e256616c59e735cc2e1ef0
apollo78124 <98apollo@naver.com>
2023-07-08 21:33:17 -0700

    commandline compilation exmaple

--- GIS.cpp
+++ GIS.cpp
@@ -3,6 +3,13 @@
 #include "Logger.h"
 using namespace GIS;
 
+/**
+ *
+Commandline execution example
+ g++ GIS.cpp CommandProcessor.cpp World.cpp HashTable.cpp BufferPool.cpp GISRecord.cpp Logger.cpp PRQuadtree.cpp -o GIS
+GIS ./Files/database.txt ./Files/script01.txt ./Files/log.txt
+ */
+
 int main(int argc, char* argv[]) {
 
     CommandProcessor* commandprocessor1 = new CommandProcessor(argv[1], argv[2], argv[3]);
